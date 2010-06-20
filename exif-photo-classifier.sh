ed7d3be1324a826ce4cb83b793b8012259a0c98b
mlvincent@gmail.com <mlvincent@gmail.com@0e8dd8fe-8dbb-9f82-ef39-5abdb86c47d6>
2010-06-20 02:53:40 +0000

    version fonctionelle, il reste a faire : verifier taille fichier si nom identique, faire detox 'inline', faire tests de tout les cas possibles...

--- exif-photo-classifier.sh
+++ exif-photo-classifier.sh
@@ -7,8 +7,22 @@
 
 STARTGLOBAL=$(date +%s.%N)
 
+CP=cp
+JHEAD=jhead
+SSDEEP=ssdeep
+MD5SUM=md5sum
+RM=/bin/rm
+
 nbrFichierTraites=0;
+nbrFichierCopie=0;
+nbrFichierRenomme=0;
+nbrFichierEfface=0;
+
 SRC=$1
+
+OUTILS=${2:-"0"}
+DEPLACE=${3:-"0"}
+
 
 SSDEEP=/usr/local/bin/ssdeep
 [ ! -x "${SSDEEP}" ] && echo "ERREUR : ${SSDEEP} n'esxiste pas" && exit;
@@ -25,7 +39,12 @@ detox -r -v -s lower "${SRC}"
 ########################################################################
 function effacer {
 
-	 echo "INFO  : effacer";
+	 fichier_1=${1}
+	 echo "INFO  : effacer()  : debut traitement de $fichier_1";
+
+
+	 $RM -f $fichier_1
+
 	 if [ $? -ne 0 ]; then
 		  echo
 		  echo "Directory does not exists"
@@ -33,36 +52,27 @@ function effacer {
 		  echo
 		  exit 0
 	 fi
+
+	 let nbrFichierEfface++
 	 return 1
 }
-
-
-
-function deplacer {
-
-	 echo "INFO  : deplacer";
-	 if [ $? -ne 0 ]; then
-		  echo
-		  echo "Directory does not exists"
-		  echo "Please try again."
-		  echo
-		  exit 0
-	 fi
-	 return 1
-}
-
 
 
 function copier {
 
-	 echo "INFO  : copier";
+	 fichier_1=${1}
+	 fichier_2=${2}
+
+	 echo "INFO  : copier()  : debut traitement de $fichier_1 et $fichier_2";
+
+	 $CP -fp $fichier_1 $fichier_2
+
 	 if [ $? -ne 0 ]; then
-		  echo
-		  echo "Directory does not exists"
-		  echo "Please try again."
-		  echo
+		  echo "ERREUR : copier() : $fichier_1 et $fichier_2";
 		  exit 0
 	 fi
+
+	 let nbrFichierCopie++
 	 return 1
 }
 
@@ -70,30 +80,39 @@ function copier {
 
 function renommer {
 
-	 echo "INFO  : renommer";
-	 if [ $? -ne 0 ]; then
-		  echo
-		  echo "Directory does not exists"
-		  echo "Please try again."
-		  echo
-		  exit 0
-	 fi
-	 return 1
+	 fichier_1=${1}
+	 fichier_dest=(`basename ${2}`)
+	 rep_dest=${2}
+
+	 echo "INFO  : renommer()  : debut traitement de $fichier_1";
+	 #copier $fichier_1 $fichier_1.`date +%d-%b-%Y_%H:%M:%S`.bak
+	 copier $fichier_1 $rep_dest/$fichier_dest.`date +"%dj%mm%Ya_%Hh%Mm%Ss"  | tr \'[:upper:]\' \'[:lower:]\'`.bak
+
+	 let nbrFichierRenomme++
+
+	 return $?
 }
 
 
 function signatures_md5_sont_identiques {
 
-	 fichier_1=(`md5sum ${1}  2>/dev/null | cut -c1-32 `)
-	 fichier_2=(`md5sum ${2}  2>/dev/null | cut -c1-32 `)
+	 fichier_1=${1}
+	 fichier_2=${2}
+
+	 md5fichier_1=(`md5sum ${fichier_1}  2>/dev/null | cut -c1-32 `)
+	 md5fichier_2=(`md5sum ${fichier_2}  2>/dev/null | cut -c1-32 `)
 
 
-	 echo "INFO  : signatures_md5_sont_identiques()  : debut traitement de $1 et $2";
 
-	 if [ "${fichier_1}" == "${fichier_2}" ]
+
+	# echo "INFO  : signatures_md5_sont_identiques()  : debut traitement de $fichier_1 et $fichier_2";
+
+	 if [ "${md5fichier_1}" == "${md5fichier_2}" ]
 	 then
+		  echo "INFO  : signatures_md5_sont_identiques()  : $fichier_1 [${md5fichier_1}] === $fichier_2 [${md5fichier_2}]";
 		  return 1;
 	 else
+		  echo "INFO  : signatures_md5_sont_identiques()  : $fichier_1 [${md5fichier_1}] !== $fichier_2 [${md5fichier_2}]";
 		  return 0;
 	 fi
 }
@@ -105,16 +124,21 @@ function signatures_SSD_sont_identiques {
 	 fichier_1=${1}
 	 fichier_2=${2}
 
-	 echo "INFO  : signatures_SSD_sont_identiques()  : debut traitement de $1 et $2";
+	 ssdeepres=-1
+	 limite=100
+
+	 echo "INFO  : signatures_SSD_sont_identiques()  : debut traitement de $fichier_1 et $fichier_2";
 
 	 ssdeep -s -b ${fichier_1} > /tmp/sig.txt
 
-	 ssdeepres=(`ssdeep -bm /tmp/sig.txt ${fichier_2} | sed -r "s/.*\(([0-9]*)\)/\\1/"`)
+	 ssdeepres=(`ssdeep -bm /tmp/sig.txt ${fichier_2}  2>/dev/null | sed -r "s/.*\(([0-9]*)\)/\\1/"`)
 
-    if(( ssdeepres > 98 )) # [ $ssdeepres -gt 98 ]
+    if [ $ssdeepres -ge $limite ]
 	 then
-		  return 0
+		  echo "INFO  : signatures_SSD_sont_identiques()  : 1 ssdeep retourne $ssdeepres pour ${md5fichier_1} et ${md5fichier_2} et limite = $limite";
+		  return 1
 	 else
+		  echo "INFO  : signatures_SSD_sont_identiques()  : 0 ssdeep retourne $ssdeepres pour ${md5fichier_1} et ${md5fichier_2} et limite = $limite";
 		  return 0
 	 fi
 
@@ -129,7 +153,7 @@ function noms_de_fichier_sont_identiques {
 	 fichier_2=(`basename ${2}`)
 
 
-	 echo "INFO  : noms_de_fichier_sont_identiques() : debut traitement de $fichier_1 $fichier_2";
+	 echo "INFO  : noms_de_fichier_sont_identiques() : debut traitement de $fichier_1 et $fichier_2";
 
 	 if [ "${fichier_1}" == "${fichier_2}" ]
 	 then
@@ -141,6 +165,14 @@ function noms_de_fichier_sont_identiques {
 
 
 
+# bash trap command
+trap bashtrap INT
+# bash trap function is executed when CTRL-C is pressed:
+# bash prints message => Executing bash trap subrutine !
+bashtrap()
+{
+    echo "INFO  : CTRL+C Detected !...executing bash trap !"
+}
 
 
 
@@ -151,9 +183,7 @@ function noms_de_fichier_sont_identiques {
 # code principal
 ########################################################################
 
-
-
-for DIR in `find "${SRC}" -type d -depth -print 2> /dev/null`;
+for DIR in `find "${SRC}" -depth -type d -print 2> /dev/null`;
 # -depth = :
 #./2007/12/15
 #./2007/12
@@ -207,12 +237,12 @@ do
 
  	         # pour commencer on affecte a notre tableau arrTS les timestamps EXIF
 				# trouve avec soit jhead soit exiftool
-				if [ "${2}" == "1" ] # si le 2e param est 1, on utilise exiftool, sinon jhead
+				if [ "${OUTILS}" == "1" ] # si le param OUTILS est 1, on utilise exiftool, sinon jhead
 				then
 					 arrTS=(`exiftool -e --fast -CreateDate -DateTimeOriginal -FileModifyDate \
 					      -S -d "%s" ${fichierEnTraitement}  | \
                     sed -r "s/.*: ([0-9:]*).*/\1/"`);
-					 echo "INFO  : traitement exiftooljhead : $(echo "$(date +%s.%N) - $START" | bc) secondes";
+					 echo "INFO  : traitement exiftool : $(echo "$(date +%s.%N) - $START" | bc) secondes";
 				else
 					 jheadTS=(`jhead -q  ${fichierEnTraitement} 2>/dev/null| grep "Date/Time" | \
 					           sed -r "s/.*: ([0-9:]*).*/\1/" | tr -d ':' `)
@@ -256,8 +286,8 @@ do
      	          # on cree le repertoire DEST
 					 if [ ! -d "${DEST}" ]; then
 						  mkdir -p ${DEST} ;
+						  echo "INFO  : traitement mkdir ${DEST} : $(echo "$(date +%s.%N) - $START" | bc) secondes";
 					 fi
-					 echo "INFO  : traitement mkdir ${DEST} : $(echo "$(date +%s.%N) - $START" | bc) secondes";
 				else
 					 echo "ERREUR : pas de timestamp pour : ${fichierEnTraitement} ... fin du programme.";
                 exit;
@@ -272,70 +302,99 @@ do
 				echo "INFO  : on s'apprete a copie/deplace ceci : [$md5FichierenTraitement] " \
 				              `ls -al ${fichierEnTraitement} 2>/dev/null`
 
-	         # et on s'apprete a copier par dessus :
-				md5FichierExistant=`md5sum ./${DEST}/${arrRepertoire[$i]}  2>/dev/null | cut -c1-32 `
-				echo "INFO  : sur ceci : [$md5FichierExistant] " `ls -al ./${DEST}/${arrRepertoire[$i]} \
-				              2>/dev/null`
+				 if [ ! -e "./${DEST}/${arrRepertoire[$i]}" ]; then
 
 
+	 					# copier le nouveau fichier ds DEST
+					  copier  ${fichierEnTraitement}  ./${DEST}/${arrRepertoire[$i]}
 
-				signatures_md5_sont_identiques ${fichierEnTraitement} ./${DEST}/${arrRepertoire[$i]}
-				md5retcode=$?
+					  if [ "${DEPLACE}" == "1" ]
+					  then
+							effacer  ${fichierEnTraitement}
+					  fi
 
-				if [ "$md5retcode" == "1" ]
-				then
-					 # effacer ou ignorer fichier source, car md5 identique = fichiers identiques
-					 effacer  ${fichierEnTraitement}
+				 else
 
-				elif [ "$md5retcode" == "0" ]
-				then
-					 # sig md5 sont differentes, on va test plus loin avec ssdeep
-					 signatures_SSD_sont_identiques  ${fichierEnTraitement} \
-					            ./${DEST}/${arrRepertoire[$i]}
-					 ssdretcode=$?
+				     # cas ou le fichier existe deja ds la DEST
+	              # et on s'apprete a copier par dessus :
+					  md5FichierExistant=`md5sum ./${DEST}/${arrRepertoire[$i]}  2>/dev/null | cut -c1-32 `
+					  echo "INFO  : sur ceci : [$md5FichierExistant] " `ls -al ./${DEST}/${arrRepertoire[$i]} \
+				         2>/dev/null`
 
-					 if [ "$ssdretcode" == "1" ]
-					 then
-					     # effacer ou ignorer fichier source, car ssdeep identique = fichiers identiques
-						  effacer  ${fichierEnTraitement}
+					  signatures_md5_sont_identiques ${fichierEnTraitement} ./${DEST}/${arrRepertoire[$i]}
+					  md5retcode=$?
 
-					 elif [ "$ssdretcode" == "0" ]
-					 then
-						  # sig ssdeep sont differentes, on va test plus loin avec les noms de fichiers
-						  noms_de_fichier_sont_identiques  ${fichierEnTraitement} \
-						             ./${DEST}/${arrRepertoire[$i]}
-						  nomretcode=$?
+					  if [ "$md5retcode" == "1" ]
+					  then
 
-						  if [ "$nomretcode" == "1" ]
-						  then
-								renommer  ${fichierEnTraitement}  ./${DEST}/${arrRepertoire[$i]}
-								copier  ${fichierEnTraitement}  ./${DEST}/${arrRepertoire[$i]}
-								if [ "${param-move}" == "0" ]
-								then
-									 effacer  ${fichierEnTraitement}  ./${DEST}/${arrRepertoire[$i]}
-								fi
-						  elif [ "$nomretcode" == "0" ]
-						  then
-								# noms de ficheirs sont differents
-								copier  ${fichierEnTraitement}  ./${DEST}/${arrRepertoire[$i]}
-								if [ "${param-move}" == "0" ]
-								then
-									 effacer  ${fichierEnTraitement}  ./${DEST}/${arrRepertoire[$i]}
-								fi
-						  else
-								# sig ssdeep sont egales
-								echo "erreur ds noms_de_fichier_sont_identiques [$?]"
-						  fi
-					 else
-						  # sig ssdeep sont egales
-						  echo "erreur ds signatures_SSD_sont_identiques [$?]"
-					 fi
-				else
-					 # erreur
-					 echo "erreur ds signatures_md5_sont_identiques [$?]"
- 				fi
+					      # effacer ou ignorer fichier source, car md5 identique = fichiers identiques
+							if [ "${DEPLACE}" == "1" ]
+							then
+								 effacer  ${fichierEnTraitement}
+							fi
 
+					  elif [ "$md5retcode" == "0" ]
+					  then
 
+ 					      # sig md5 sont differentes, on va test plus loin avec ssdeep
+							signatures_SSD_sont_identiques  ${fichierEnTraitement} \
+					          ./${DEST}/${arrRepertoire[$i]}
+							ssdretcode=$?
+
+							if [ "$ssdretcode" == "1" ]
+							then
+					          # effacer ou ignorer fichier source, car ssdeep identique = fichiers identiques
+								 if [ "${DEPLACE}" == "1" ]
+								 then
+									  effacer  ${fichierEnTraitement}
+								 fi
+
+							elif [ "$ssdretcode" == "0" ]
+							then
+						   # sig ssdeep sont differentes, on va test plus loin avec les noms de fichiers
+								 noms_de_fichier_sont_identiques  ${fichierEnTraitement} \
+						           ./${DEST}/${arrRepertoire[$i]}
+								 nomretcode=$?
+#### attention : faire test a savoir si A a une taille plus petite que A`
+								 if [ "$nomretcode" == "1" ]
+								 then
+
+								     # faire backup du fichier existant
+									  renommer  ${fichierEnTraitement} ./${DEST}/
+
+								     # copier le nouveau fichier ds DEST
+									  copier  ${fichierEnTraitement}  ./${DEST}/${arrRepertoire[$i]}
+
+									  if [ "${DEPLACE}" == "1" ]
+									  then
+											effacer  ${fichierEnTraitement}
+									  fi
+								 elif [ "$nomretcode" == "0" ]
+								 then
+
+								     # noms de ficheirs sont differents
+									  copier  ${fichierEnTraitement}  ./${DEST}/${arrRepertoire[$i]}
+									  if [ "${DEPLACE}" == "1" ]
+									  then
+											effacer  ${fichierEnTraitement}
+									  fi
+								 else
+
+								     # sig ssdeep sont egales
+									  echo "erreur ds noms_de_fichier_sont_identiques [$?]"
+								 fi
+							else
+
+						       # sig ssdeep sont egales
+								 echo "erreur ds signatures_SSD_sont_identiques [$?]"
+							fi
+					  else
+					      # erreur
+							echo "erreur ds signatures_md5_sont_identiques [$?]"
+ 					  fi
+
+					 # echo ""
+					 # echo "INFO  :       dans ${DEST} : [$md5FichierExistant] " `ls -al ./${DEST}/${arrRepertoire[$i]}`
 
 
 # 		      # debut d'une section de chronometrage
@@ -344,7 +403,7 @@ do
 
 # 		      # on deplace/copie le fichier ICI (si $3 vaut 1, on fait MV, sinon CP /; par defaut c'est donc CP)
 # 		      # exif-photo-classifier.sh src 0 1
-# 				if [ "${3}" == "1" ]
+# 				if [ "${DEPLACE}" == "1" ]
 # 				then
 # 					 mv ${fichierEnTraitement} ${DEST}
 # 					 echo "INFO  : deplacement de : ${fichierEnTraitement} vers : ${DEST} : \
@@ -356,14 +415,12 @@ do
 # 				fi
 
 
-
+				 fi
 
 	         # compteur de fichier traites
 				let nbrFichierTraites++
 
 		      # info pour debug : ls du fichier post deplacement
-				echo -n "INFO  :       dans ${DEST} : [$md5FichierExistant] " `ls -al ./${DEST}/${arrRepertoire[$i]}`
-				echo "";
 				echo "INFO  : traitement de ${fichierEnTraitement} : $(echo "$(date +%s.%N) - $STARTFICHIER"\
 				               | bc) secondes";echo "";echo "";
 
@@ -382,7 +439,10 @@ done
 
 
 # output d'info/debug
-echo "INFO  : ${nbrFichierTraites} fichiers ont ete copier/deplacer.";
+echo "INFO  : ${nbrFichierTraites} fichiers ont ete traites.";
+echo "INFO  : ${nbrFichierCopie} fichiers ont ete copie/deplace.";
+echo "INFO  : ${nbrFichierEfface} fichiers ont ete efface.";
+echo "INFO  : ${nbrFichierRenomme} fichiers ont ete renommer";
 echo "INFO  : traitement TOTAL : $(echo "$(date +%s.%N) - $STARTGLOBAL" | bc) secondes";
 
 
